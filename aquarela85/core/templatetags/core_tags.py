01b901f9e2fdc978ba6a9674f64ea8e3a8335008
Diego Masin <diego.masin@defensoria.ce.def.br>
2018-07-03 18:55:22 -0300

    Montagem do Template.
    Criado o template base do sistema e o template de login. Inserida corretamente as importações de css e js.

---
+++ aquarela85/core/templatetags/core_tags.py
@@ -0,0 +1,31 @@
+# coding: utf-8
+from django import template
+from django.utils.html import strip_tags
+
+from aquarela85.core.masks import Money
+
+register = template.Library()
+
+
+@register.filter()
+def to_mask_money(value):
+    return Money().format(value) if value else 'R$ 0,00'
+
+
+@register.filter()
+def format_positivo_negativo(value):
+    formato_cor = 'success' if value >= 0 else 'danger'
+
+    return formato_cor
+
+
+@register.filter()
+def format_status_conta(value):
+    formato_cor = 'success' if value else 'danger'
+
+    return formato_cor
+
+
+@register.filter(is_safe=True)
+def message_erro_custom(value):
+    return strip_tags(value).replace('dict_values', '').replace('([[', '').replace(']])', '').replace('\'', '').replace('.', '. ')
