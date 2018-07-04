13ed53427d35fa7889e737d4355764387d1df448
Diego Masin <diego.masin@defensoria.ce.def.br>
2018-07-03 21:59:22 -0300

    Apenas Login Funcionando.
    Falta dar manutenção em todos os erros de tela. E ajustes de outros detalhes de layout para finalizar.

--- aquarela85/urls.py
+++ aquarela85/urls.py
@@ -21,6 +21,6 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('aquarela85.core.urls')),
-    url(r'^', include('aquarela85.usuarios.urls')),
+    url(r'^', include('aquarela85.usuario.urls')),
     ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL,
                                                                                  document_root=settings.MEDIA_ROOT)
